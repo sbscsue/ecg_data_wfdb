60d524cca80fb7586709c36f0a569b2aae79735f
sebinkim <sbscsue@gmail.com>
2021-08-19 17:33:54 +0900

    2nd model ,but acc is very low

--- use/random_file.py
+++ use/random_file.py
@@ -2,33 +2,66 @@ import os
 from shutil import copy
 import random
 
-#segment file path
-input_path = "C:\\seb\\ecg_detection\\segment\\out2"
-#random segment file output
-output_path = "C:\\seb\\ecg_detection\\segment\\set\\0101_ex2"
+
+
+#pick random ecg by pick num
+#save that folder1(sort annotation) folder2(all annotation)
+def random_ecg(pick_num,input_folder,output_folder):
+    print("random_ecg")
+    ann_folder = os.listdir(input_folder)
+
+    out1=output_folder +"\\0_ann"
+    out2=output_folder+"\\1_all"
+
+    if(os.path.isdir(out1)==False):
+        print("in")
+        dst_path_2 = out2
+        os.makedirs(dst_path_2)
+
+        for p in ann_folder:
+            src_path = input_folder+"\\"+p
+
+            dst_path_1 = out1+"\\"+p
+            os.makedirs(dst_path_1)
+            
+            seg = os.listdir(src_path)
+            seg_num = len(seg)
+
+            if seg_num < pick_num:
+                ran = random.sample(range(seg_num),seg_num)
+            else:
+                ran = random.sample(range(seg_num),pick_num)
+
+            for pick in ran:
+                copy(src_path+"\\"+seg[pick],dst_path_1+"\\"+seg[pick])
+                copy(src_path+"\\"+seg[pick],dst_path_2+"\\"+seg[pick])
 
 
 
-pick_num = 70
 
-ann_folder = os.listdir(input_path)
-for p in ann_folder:
-    src_path = input_path+"\\"+p
-    dst_path = output_path+"\\"+p
+def random_ecg_all(pick_num,input_folder,output_folder):
+    ann_folder = os.listdir(input_folder)
 
-    os.mkdir(dst_path)
-    
-    seg = os.listdir(input_path+"\\"+p)
-    seg_num = len(seg)
-
-    if seg_num < pick_num:
-        ran = random.sample(range(seg_num),seg_num)
-    else:
-        ran = random.sample(range(seg_num),pick_num)
-
-    for pick in ran:
-        copy(src_path+"\\"+seg[pick],dst_path+"\\"+seg[pick])
+    output_folder = output_folder + "\\1_all"
 
 
+    if(os.path.isdir(output_folder)==False):
+        dst_path = output_folder
+        os.makedirs(dst_path)
 
+        for p in ann_folder:
+            src_path = input_folder+"\\"+p
+        
+
+            
+            seg = os.listdir(input_folder+"\\"+p)
+            seg_num = len(seg)
+
+            if seg_num < pick_num:
+                ran = random.sample(range(seg_num),seg_num)
+            else:
+                ran = random.sample(range(seg_num),pick_num)
+
+            for pick in ran:
+                copy(src_path+"\\"+seg[pick],dst_path+"\\"+seg[pick])
 

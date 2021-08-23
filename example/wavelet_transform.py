7c0d0610e81012e38eedebb075f55895bfbbb3d2
sebinkim <sbscsue@gmail.com>
2021-08-23 12:40:45 +0900

    wt example

--- example/wavelet_transform.py
+++ example/wavelet_transform.py
@@ -6,15 +6,33 @@ import os
 import sys
 import pickle 
 
-import wfdb 
 import pywt as wt
 
-sys.path.append(os.getcwd())
+sys.path.append(os.getcwd()+"\\ecg_data_wfdb")
 import use.segment.open as so
 
 
-path="C:\\seb\\ecg_detection\\segment\\01)no_processing\\out2\\F\\109_49.txt"
+path="C:\\sebin\\ecg\\sample\\113_2.txt"
 ecg= so.file_open(path)
 
 
+
+
+wt = wt.wavedec(ecg['record'],'db1',level=3)
+
+plt.subplot(5,1,1)
 plt.plot(ecg['record'])
+
+plt.subplot(5,1,2)
+plt.plot(wt[3])
+
+plt.subplot(5,1,3)
+plt.plot(wt[2])
+
+plt.subplot(5,1,4)
+plt.plot(wt[1])
+
+plt.subplot(5,1,5)
+plt.plot(wt[0])
+
+plt.show()

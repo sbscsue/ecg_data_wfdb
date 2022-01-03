f6b9d966692d137843f7cc7431a76716d3931984
sebinkim <sbscsue@gmail.com>
2022-01-03 20:30:34 +0900

    model2_binary - finish [use/dataPick,dataSize model/study,make ] ing [use/dataopen]

--- use/mitbih_ecg/form.py
+++ use/mitbih_ecg/form.py
@@ -1,58 +1,62 @@
+from genericpath import isdir
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd 
 
 import os
+from os import makedirs
+from os.path import isdir
+
 import sys
 import pickle 
 
 import wfdb 
 
+
+#annotation code 
+#https://archive.physionet.org/physiobank/annotations.shtml
+
+
+#lbbb rbbb 제외
 class ecg_segment:
-    def __init__( self, file_path, ver="mitbih", channel=[0] ,sampto=None, seg_size=144 ):
-        #annotation / 주의(file 이름은 / 가 안된다!)
-        self.mit_bih_ann = ['L','N','R','e','j','A','J','S','a','E','V','F','/','f','Q']   
-        self.mit_bih_ann_to_file = ['L','N','R','e','j','A','J','S','a','E','V','F','m','f','Q']  
-        self.aami_ann = ['N','S','V','F','Q']
-        self.mit_to_aami = { 
+    def __init__( self, file_path, ver="mitbih", channel=[0] , seg_size=144 ,sampto=None):
+        # / -> m (저장 문제로)
+        self.mit_bih = ['L','N','R','S','E','A','J',
+                        'V','F','/','Q',
+                        'e','a','j','f']   
+        self.aami = { 
                         'N':['L','N','R','e','j'],
                         'S':['A','J','S','a'],
                         'V':['E','V'],
                         'F':['F'],
                         'Q':['/','f','Q']
                         }
-
+        self.binary = {
+            'normal': ['N'],
+            'abnormal': ['A','a','J','S','V','r','F','e','j','n','E','f'],
+            'unclassification':['Q','?']
+        }
                         
-        #1. save default value
+
         self.ver = ver
+        if(ver not in ['aami','mitbih','binary']):
+            raise NameError('not support annotation')
         self.file_name = file_path.split("\\")[-1]
         self.file_path = file_path
 
         print(self.file_name)
 
-        #2. record value , annotation value 
         self.record = wfdb.rdsamp(self.file_path,channels=channel,sampto = sampto)[0].flatten()
         #annotation  = annotation wfdb class / sample / symbol / value 
         self.annotation = wfdb.rdann(self.file_path,'atr',summarize_labels=True,sampto = sampto)
     
+
         self.tmp = self.re_ann()
-
-        #3. segement
-        #divide beat and non_beat(wave form)
-        if ver=='aami':
-            self.beat,self.non_beat = self.set_annotation_aami()
-        elif ver=='mitbih':
-            self.beat,self.non_beat = self.set_annotation_mitbih()
-        else:
-            raise NameError('not support annotation')
-
-        #seg = use beat
+        self.beat , self.non_beat = self.set_annotation()
         self.seg = self.set_segment(seg_size)
     
     
         
-    
-
 
     #ecg
     #plot ecg 
@@ -76,8 +80,6 @@ class ecg_segment:
 
 
 
-
-
     def re_ann(self):
         n = len(self.annotation.sample)
 
@@ -95,7 +97,7 @@ class ecg_segment:
         return tmp
 
     #set 
-    def set_annotation_aami(self):
+    def set_annotation(self):
         beat = np.empty([])
         non_beat = np.empty([])
 
@@ -103,44 +105,44 @@ class ecg_segment:
         cnt = 0
 
         tmp = self.tmp
-        for i in range(n):
-            if tmp[i][1] in self.mit_bih_ann:
-                for j in self.mit_to_aami:
-                    if tmp[i][1] in self.mit_to_aami[j]:
-                        tmp[i][1] = j
-                        beat = np.append(beat,tmp[i])
-            else:
-                non_beat = np.append(non_beat,tmp[i])
-        
-        beat = beat[1:].reshape((-1,3))
-        non_beat = non_beat[1:].reshape((-1,3))
 
-        return beat,non_beat
-    
-    def set_annotation_mitbih(self):
-        beat = np.empty([])
-        non_beat = np.empty([])
-
-        n = len(self.tmp)
-        cnt = 0
-
-        tmp = self.tmp
-        for i in range(n):
-            if tmp[i][1] in self.mit_bih_ann:
-                if tmp[i][1] == "/":
-                    tmp[i][1] = "m"
-                    beat = np.append(beat,tmp[i])
+        if self.ver=='aami':   
+            tmp = self.tmp
+            for i in range(n):
+                if tmp[i][1] in self.mit_bih:
+                    for j in self.aami:
+                        if tmp[i][1] in self.aami[j]:
+                            tmp[i][1] = j
+                            beat = np.append(beat,tmp[i])
                 else:
-                    beat = np.append(beat,tmp[i])
-            else:
-                non_beat = np.append(non_beat,tmp[i])
-        
+                    non_beat = np.append(non_beat,tmp[i])
+        elif self.ver=='mitbih':
+            for i in range(n):
+                if tmp[i][1] in self.mit_bih:
+                    if tmp[i][1] == "/":
+                        tmp[i][1] = "m"
+                        beat = np.append(beat,tmp[i])
+                    else:
+                        beat = np.append(beat,tmp[i])
+                else:
+                    non_beat = np.append(non_beat,tmp[i])
+
+        elif self.ver == 'binary':
+            for i in range(n):
+                if tmp[i][1] in self.mit_bih:
+                    for j in self.binary:
+                        if tmp[i][1] in self.binary[j]:
+                            tmp[i][1] = j
+                            beat = np.append(beat,tmp[i])
+                else:
+                    non_beat = np.append(non_beat,tmp[i])
+
         beat = beat[1:].reshape(-1,3)
         non_beat = non_beat[1:].reshape(-1,3)
         
         return beat,non_beat
-    
 
+ 
 
     def set_segment(self,window):
         segment = []
@@ -164,69 +166,57 @@ class ecg_segment:
 
     
         
-#output segment to python dictionary 
-    def output_segment_aami(self,dir):
-    
+#mitbih 파일 경로 겹치는 오류 
+    def output_segment(self,path,img = False):
+        ann = []
+        if self.ver=='mitbih':
+            ann = self.mit_bih
+        elif self.ver=='aami':
+            ann = list(self.aami.keys())
+        elif self.ver == 'binary':
+            ann = list(self.binary.keys())
+
+
         #type1:101,102
-        path_1 = dir+"\\type1\\"+self.file_name
-        if not os.path.exists(path_1):
-            os.makedirs(path_1)
-        
-        if not os.path.exists(path_1+"\\N"):
-            for p in self.aami_ann:
-                os.makedirs(path_1+"\\"+p)
+        path1 = path+"\\"+"files"+"\\"+str(self.file_name)
+        path1_1 = path1+"\\"+"all"
+        path1_2 = path1+"\\"+"ann"
+        if isdir(path1) == False:
+            makedirs(path1_1)
+            for p in ann:
+                makedirs(path1_2+"\\"+p)
         
 
         #type2:n,s,q,r...
-        path_2 = dir+'\\type2'
-        if not os.path.exists(path_2):
-            os.makedirs(path_2)
-        if not os.path.exists(path_2+"\\N"):
-            for p in self.aami_ann:
-                os.makedirs(path_2+"\\"+p)
-             
-        n = len(self.seg)
+        path2 = path+"\\"+"annotation"
+        if isdir(path2) == False:
+            for p in ann:
+                makedirs(path2+"\\"+p+"\\csv")
+                if(img == True):
+                    makedirs(path2+"\\"+p+"\\img")
 
-        for i in range(n):
+        
+        for i in range(len(self.seg)):
             record = self.seg[i]['record']
             ann = self.seg[i]['annotation']
-
-            data =  pd.DataFrame(np.append(record,ann))
             
-            name = str(self.file_name)+"_"+str(ann)+"_"+str(i)
+            data = np.append(record,ann)
+            data = pd.DataFrame(data)
+
+            name = str(self.file_name)+"_"+str(i)+".csv"
+
+            data.to_csv(path1_1+"\\"+name,index=False)
+            data.to_csv(path1_2+"\\"+ann+"\\"+name,index=False)
+            data.to_csv(path2+"\\"+ann+"\\"+"csv"+"\\"+name,index=False)
+
+            if(img == True):
+                plt.plot(record)
+                plt.savefig(path2+"\\"+ann+"\\"+"img"+"\\"+name+".jpg")
+                plt.cla() 
+
             
-            path = path_1+"\\"+ann+"\\"+name+".csv"
-            data.to_csv(path,header=False,index=False)
-            path = path_2+"\\"+ann+"\\"+name+".csv"
-            data.to_csv(path,header=False,index=False)
 
 
-    
-    
-    def output_segment_mitbih(self,path):
-        p = path+"\\"+str(self.file_name)
-        csv_p = p+"\\"+"csv"
-        img_p = p+"\\"+"img"
-        
-        print(p)
-        print(os.path.isdir(p))
-        if os.path.isdir(p)==False:
-            os.makedirs(csv_p)
-            os.makedirs(img_p)
-        else:
-            return -1
-
-        for i in range(len(self.seg)):
-            print(i)
-            ann = self.seg[i]['annotation']
-            ecg = pd.DataFrame(self.seg[i]['record'])
-
-            ecg.to_csv(csv_p+"\\"+str(ann)+"_"+str(i))
-            plt.plot(ecg)
-            plt.savefig(img_p+"\\"+str(ann)+"_"+str(i))
-
-            plt.cla() 
-        
 
             
     
--- use/segment/open.py
+++ use/segment/open.py
@@ -6,22 +6,35 @@ import matplotlib.pyplot as plt
 
 from os import listdir
 
-#file open one file 
-def ecgtodf(path,size):
-
+def all_check(path):
+    ann = []
+    cnt = []
     dir = listdir(path)
+    for i in range(len(dir)):
+        f = 0
+        for j in listdir(path+"\\"+dir[i]+"\\"+"csv"):
+            f+=1
+        ann.append(dir[i])
+        cnt.append(f)
+    return ann,cnt
+
+#annotation 별 open _ dtframe ( annotation 별로 저장되어있는 파일로)
+#주의 ! data 부분 1번째 배열부터 뽑아옴
+def toDataframe(p):
+    path = p+"\\"+"csv"
+    dir = listdir(path)
+    
+
+    df = pd.DataFrame(np.empty((1,289)))
+    print(df)
+    for f in dir:
+        print(path+"\\"+f)
+        data = pd.read_csv(path+"\\"+f,header=None)[1:].T
+        print(data)
+        df.append(df,data)
+        
+    return df
+        
 
 
-    all = np.ndarray(1)
-    n=0
-
-    for p in dir:
-        print(p)
-        data = pd.read_csv(path+"\\"+p,header=None)
-        all =  np.append(all,data)
-        n = n+1
-    all = all[1:]
-    all=np.resize(all,(n,size))
-
-    return all
 
